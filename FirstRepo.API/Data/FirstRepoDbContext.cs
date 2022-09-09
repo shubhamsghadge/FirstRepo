a33738cef540a9836f9bb82f1eeb5e2333a102f6
ShubhamGhadge <shubhamsghadge@gmail.com>
2022-09-09 15:21:17 +0530

    MIgration Completed

---
+++ FirstRepo.API/Data/FirstRepoDbContext.cs
@@ -0,0 +1,18 @@
+﻿using FirstRepo.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstRepo.API.Data
+{
+    public class FirstRepoDbContext : DbContext
+    {
+        public FirstRepoDbContext(DbContextOptions<FirstRepoDbContext> options):base(options)
+        {
+
+        }
+        // Create Regions Table for us  if it doesn't exist  in database
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+
+    }
+}
---
+++ FirstRepo.API/Models/Domain/Region.cs
@@ -0,0 +1,20 @@
+﻿namespace FirstRepo.API.Models.Domain
+{
+    public class Region
+    {
+        public Guid id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double Area { get; set; }
+        public double Lat { get; set; }
+        public double Long { get; set; }
+        public long Population { get; set; }
+
+        // Navigation Property 
+
+        // One Region has Multiple Walks
+        public IEnumerable<Walk> Walks { get; set; }
+
+
+    }
+}
